1d487e914697cc980cc3ca532b32a70dfc9ac5b4
yaser <yaserbahrami@outlook.com>
2017-08-27 04:19:35 +0430

    main view controller is done

--- TvMazeApp/ViewController.swift
+++ TvMazeApp/ViewController.swift
@@ -1,25 +1,32 @@
-//
-//  ViewController.swift
-//  TvMazeApp
-//
-//  Created by yaser on 8/17/17.
-//
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let deliveryVC: ListViewController = ListViewController()
+        self.view.backgroundColor = UIColor.white
+        UINavigationBar.appearance().barTintColor = UIColor.black
+
+        let BaseNavigation = UINavigationController(rootViewController: deliveryVC)
+        self.addChildViewController(BaseNavigation)
+        self.view.addSubview(BaseNavigation.view)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
